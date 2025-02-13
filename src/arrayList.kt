9448297b1ae4e0555df441061805c3b36e19baa8
spdys <noorii549@yahoo.com>
2025-02-13 17:44:05 +0300

    Finished part 4 with array lists of car brands

---
+++ src/arrayList.kt
@@ -0,0 +1,8 @@
+val carBrands = arrayListOf("Porsche", "Ferrari", "Volkswagen")
+
+fun main() {
+    carBrands.add("Lamborghini")
+    carBrands.add("Ford")
+    carBrands.remove("Lamborghini")
+    println(carBrands)
+}
