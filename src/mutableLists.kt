b197a1bc04d1988267abf4ce82d3fd494f36030a
spdys <noorii549@yahoo.com>
2025-02-13 17:33:08 +0300

    Finished part 2 working with sets, realized that sets only keep unique numbers

--- src/mutableLists.kt
+++ src/mutableLists.kt
@@ -1,6 +1,5 @@
 val fruits = mutableListOf("Apple", "Banana", "Tomato")
 
-
 fun main() {
     fruits.add("Peach")
     fruits.removeAt(1)
---
+++ src/mutableSets.kt
@@ -0,0 +1,7 @@
+val numbers = mutableSetOf(1, 2, 3, 3, 4, 5, 5)
+
+fun main() {
+    numbers.add(3)
+    numbers.add(6)
+    println(numbers)
+}
